2f7995a5a0cd2540716dcc53c7c7004d1bf46769
Elaine Broussard <elaine@elainebroussard.com>
2016-09-11 17:05:02 -0500

    Add header, section and footer + content.

--- index.php
+++ index.php
@@ -8,7 +8,19 @@
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
-	<h1>Elaine Broussard Blank WordPress Template</h1>
-    <p>This is my WordPress template.</p>
+	<header>
+		<h1>Elaine Broussard Blank WordPress Template</h1>
+    	<p>This is my WordPress template.</p>
+	</header>
+
+	<section>
+		<h2>Section Content</h2>
+		<p>This is some cool section content.</p>
+	</section>
+
+	<footer>
+		<h2>Footer Content</h2>
+		<p>This is some cool footer content.</p>
+	</footer>
 </body>
 </html>
