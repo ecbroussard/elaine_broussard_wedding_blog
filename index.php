311689923932b7a82ca85234b4444480b11644c8
Elaine Broussard <elaine@elainebroussard.com>
2016-09-11 16:41:22 -0500

    Add link to stylesheet.

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title></title>
+	<meta charset="UTF-8">
+	<title>Elaine Broussard Blank Template</title>
+	<head>
+	<!-- Links to our style.css file -->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
-	<h1>Blank WordPress Template</h1>
+	<h1>Elaine Broussard Blank WordPress Template</h1>
     <p>This is my WordPress template.</p>
 </body>
 </html>
