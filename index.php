de95f1dff893e871e6c95f8056fdb3cc6dac7a1c
Elaine Broussard <elaine@elainebroussard.com>
2016-11-06 23:17:03 -0600

    complete styling of posts

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 								}
 							?>
 							<?php the_excerpt(); ?>
-							<a href="<?php the_permalink(); ?>">Read More</a>
+							<a class="morebutton" href="<?php the_permalink(); ?>">Read More</a>
 						</div>
 						<?php	
 					} //end while
